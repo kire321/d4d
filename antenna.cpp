559d1b3a11469bb7a805a02ebe9a4f6f6eb9d696
Daniel Tahara <dan.tahara@gmail.com>
2013-04-09 08:59:04 +0200

    Change \t to spaces

--- antenna.cpp
+++ antenna.cpp
@@ -1,7 +1,7 @@
 #include <antenna.h>
 
 Antenna Antenna::nearest(multiDimVala<float> &otherAntennas) {
-	multiDimVala<float> deltas=otherAntennas-antennas->getCopy(0,index);
-	deltas.data*=deltas.data;
-	return Antenna(&otherAntennas,argmin(deltas.sum(1)));
+    multiDimVala<float> deltas=otherAntennas-antennas->getCopy(0,index);
+    deltas.data*=deltas.data;
+    return Antenna(&otherAntennas,argmin(deltas.sum(1)));
 };
--- antenna.h
+++ antenna.h
@@ -1,14 +1,14 @@
 #include <multiDimVala.h>
 
 class Antenna {
-public:
-	multiDimVala<float> *antennas;
-	int index;
-	Antenna(multiDimVala<float> *argAntennas, int argIndex): antennas(argAntennas), index(argIndex) {assert(antennas->shape[1]==2);}
-	float lat() const {return antennas->getSingle(index,0);}
-	float lon() const {return antennas->getSingle(index,1);}
-	void print() {cout << lat() << "\t" << lon() << endl;}
-	Antenna nearest(multiDimVala<float> &otherAntennas);
-	bool operator==(const Antenna& rhs) {return this->lat()==rhs.lat() && this->lon()==rhs.lon();}
-	bool operator!=(const Antenna& rhs) {return !(*this==rhs);}
+    public:
+        multiDimVala<float> *antennas;
+        int index;
+        Antenna(multiDimVala<float> *argAntennas, int argIndex): antennas(argAntennas), index(argIndex) {assert(antennas->shape[1]==2);}
+        float lat() const {return antennas->getSingle(index,0);}
+        float lon() const {return antennas->getSingle(index,1);}
+        void print() {cout << lat() << "\t" << lon() << endl;}
+        Antenna nearest(multiDimVala<float> &otherAntennas);
+        bool operator==(const Antenna& rhs) {return this->lat()==rhs.lat() && this->lon()==rhs.lon();}
+        bool operator!=(const Antenna& rhs) {return !(*this==rhs);}
 };
